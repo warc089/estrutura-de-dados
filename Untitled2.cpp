1d04462ee97cb2b80b8bad7779eee1ef30af43ae
warc <luizdk089@gmail.com>
2026-03-04 11:09:00 -0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a[5], b[5], c[5];
+	
+	cout << "vetor a" << endl;
+	for (int i = 0; i < 5; ++i){
+		cout << "informe um valor" << endl;
+		cin >> a[i];
+	}
+	cout << "###############" << endl;
+	
+		cout << "vetor b" << endl;
+	for (int i = 0; i < 5; ++i){
+		cout << "informe um valor" << endl;
+		cin >> b[i];
+	}
+	cout << "###############" << endl;
+	
+}
+
